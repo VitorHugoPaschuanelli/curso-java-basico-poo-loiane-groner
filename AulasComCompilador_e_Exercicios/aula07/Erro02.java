45d45e91c9c698440d464021567641a2671cf93a
Vitor Hugo Paschuanelli <vitorhpaschuanelli@gmail.com>
2025-07-31 03:08:53 -0300

    Adicionando aulas anteriores

---
+++ AulasComCompilador_e_Exercicios/aula07/Erro02.java
@@ -0,0 +1,10 @@
+class Erro02 {
+	
+	public static void main(String[] args) {
+
+        //int cont; <-- ERRO
+        int cont = 1;
+
+		System.out.println("Você digitou: " + cont);
+	}
+}
---
+++ AulasComCompilador_e_Exercicios/aula07/Erro03.java
@@ -0,0 +1,8 @@
+class Erro03 {
+	
+	//public static void Main(String[] args) { <-- ERRO
+    public static void Main(String[] args) {
+		
+		System.out.println("Sem erros!");
+	}
+}
