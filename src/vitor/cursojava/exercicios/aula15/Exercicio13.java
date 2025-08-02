970fcbf3a6c34fc7ebe68ccf074f47f57358de4f
Vitor Hugo Paschuanelli <vitorhpaschuanelli@gmail.com>
2025-08-02 00:47:52 -0300

    Mais exercícios feitos

--- src/vitor/cursojava/exercicios/aula15/Exercicio13.java
+++ src/vitor/cursojava/exercicios/aula15/Exercicio13.java
@@ -1,12 +1,31 @@
 package vitor.cursojava.exercicios.aula15;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 public class Exercicio13 {
 
 	public static void main(String[] args) {
 		/* 13.Faça um Programa que leia um número e exiba o dia correspondente
 		da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve
 		aparecer valor inválido. */
-
+		
+		Scanner scan = new Scanner(System.in);
+		scan.useLocale(Locale.US);
+		
+		System.out.println("Digite um número que corresponderá a um dia da semana (1-7): ");
+		byte num = scan.nextByte();
+		
+		switch(num) {
+			
+			case 1 -> System.out.println("Domingo");
+			case 2 -> System.out.println("Segunda");
+			case 3 -> System.out.println("Terça");
+			case 4 -> System.out.println("Quarta");
+			case 5 -> System.out.println("Quinta");
+			case 6 -> System.out.println("Sexta");
+			case 7 -> System.out.println("Sábado");
+			default -> System.out.println("Valor inválido");
+		}
 	}
-
 }
--- src/vitor/cursojava/exercicios/aula15/Exercicio14.java
+++ src/vitor/cursojava/exercicios/aula15/Exercicio14.java
@@ -1,5 +1,8 @@
 package vitor.cursojava.exercicios.aula15;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 public class Exercicio14 {
 
 	public static void main(String[] args) {
@@ -13,7 +16,40 @@ public class Exercicio14 {
 		o Entre 4.0 e 6.0 D
 		o Entre 4.0 e zero E */
 		
-
+		Scanner scan = new Scanner(System.in);
+		scan.useLocale(Locale.US);
+		
+		System.out.println("Digite a primeira nota: ");
+		double nota1 = scan.nextDouble();
+		
+		System.out.println("Digite a segunda nota: ");
+		double nota2 = scan.nextDouble();
+		
+		double media = (nota1 + nota2) / 2;
+		char conceito = ' ';
+		
+		if(media >= 0 && media <= 10) {
+			if(media >= 9 || media == 10) {
+				conceito = 'A';
+			}
+			else if(media < 9 && media >= 7.5) {
+				conceito = 'B';
+			}
+			else if(media < 7.5 && media >= 6) {
+				conceito = 'C';
+			}
+			else if(media < 6 && media >= 4) {
+				conceito = 'D';
+			}
+			else if(media < 4 && media >= 0) {
+				conceito = 'E';
+			}
+			System.out.println("A média do(a) aluno(a) é: " + media);
+			System.out.println("O conceito do(a) aluno(a) é: " + conceito);
+		} 
+		else {
+			System.out.println("Média não está entre 0 e 10.");
+		}
 	}
-
 }
+	
